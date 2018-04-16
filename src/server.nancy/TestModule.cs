182205451c8f35f7b9ce2e998585c8762d140eba
novinskii <novinskii@smartdriving.io>
2018-04-16 19:41:05 +0300

    add nlog to nancy modules

--- src/server.nancy/TestModule.cs
+++ src/server.nancy/TestModule.cs
@@ -1,16 +1,22 @@
 ﻿using Nancy;
+using NLog;
 
 namespace Domain0.Nancy
 {
     public class TestModule : NancyModule
     {
-        public TestModule()
+        private readonly ILogger _logger;
+
+        public TestModule(ILogger logger)
         {
+            _logger = logger;
+
             Get("/testapimethod", ctx => TestMethod(), null, nameof(TestMethod));
         }
 
         public object TestMethod()
         {
+            _logger.Debug("TestMethod invoked");
             return HttpStatusCode.OK;
         }
     }
