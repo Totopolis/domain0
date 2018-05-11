c13b26472666797133a4702ac51f3b9577d798f9
novinskii <novinskii@smartdriving.io>
2018-05-11 17:46:35 +0300

    implement SmsRequestRepository

--- src/server/ISmsRequestRepository.cs
+++ src/server/ISmsRequestRepository.cs
@@ -4,12 +4,28 @@ using System.Threading.Tasks;
 
 namespace Domain0.Repository
 {
+    /// <summary>
+    /// Репозиторий запросов регистрации.
+    /// </summary>
     public interface ISmsRequestRepository
     {
+        /// <summary>
+        /// Сохранение запроса.
+        /// </summary>
+        /// <param name="smsRequest">запрос регистрации.</param>
+        /// <returns></returns>
         Task Save(SmsRequest smsRequest);
-
+        /// <summary>
+        /// Получение запроса по номеру телефона.
+        /// </summary>
+        /// <param name="phone">номер телефона.</param>
+        /// <returns></returns>
         Task<SmsRequest> Pick(decimal phone);
-
+        /// <summary>
+        /// Удаление запроса по номеру телефона.
+        /// </summary>
+        /// <param name="phone">номер телефона.</param>
+        /// <returns></returns>
         Task Remove(decimal phone);
     }
 }
---
+++ src/server/SmsRequestRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Domain0.Repository;
+using Domain0.Repository.Model;
+using Gerakul.FastSql;
+
+namespace Domain0.FastSql
+{
+    public class SmsRequestRepository : ISmsRequestRepository
+    {
+        private readonly string _connectionString;
+
+        public const string TableName = "[dom].[Caching]";
+
+        public SmsRequestRepository(string connectionString)
+            => _connectionString = connectionString;
+
+        public Task<SmsRequest> Pick(decimal phone)
+            => SimpleCommand.ExecuteQueryAsync<SmsRequest>(_connectionString,
+                    $"select * from {TableName} where {nameof(SmsRequest.Phone)}=@p0", phone)
+                .FirstOrDefault();
+
+        public Task Remove(decimal phone)
+            => SimpleCommand.ExecuteNonQueryAsync(_connectionString,
+                $"delete from {TableName} where {nameof(SmsRequest.Phone)}=@p0", phone);
+
+        public Task Save(SmsRequest smsRequest)
+            => MappedCommand.InsertAsync(_connectionString, TableName, smsRequest);
+
+    }
+}
