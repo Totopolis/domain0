52a4abe8147cea4ab8f1b29931c16b2a4f023f48
novinskii <novinskii@smartdriving.io>
2018-05-11 17:47:07 +0300

    implement MessageTemplateRepository

--- src/server/MessageTemplate.cs
+++ src/server/MessageTemplate.cs
@@ -4,5 +4,7 @@ namespace Domain0.Repository.Model
     public class MessageTemplate
     {
         public string Template { get; set; }
+
+        public string Name { get; set; }
     }
 }
---
+++ src/server/MessageTemplateRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain0.Repository;
+using System.Linq;
+using System.Threading.Tasks;
+using Gerakul.FastSql;
+using Domain0.Repository.Model;
+
+namespace Domain0.FastSql
+{
+    public class MessageTemplateRepository : IMessageTemplateRepository
+    {
+        private readonly string _connectionString;
+
+        public const string TableName = "[dom].[Message]";
+
+        public MessageTemplateRepository(string connectionString)
+            => _connectionString = connectionString;
+
+        public Task<string> GetWelcomeTemplate()
+            => SimpleCommand.ExecuteQueryFirstColumnAsync<string>(_connectionString,
+                    $"select {nameof(MessageTemplate.Template)} from {TableName} where {nameof(MessageTemplate.Name)}='WelcomeTemplate'")
+                .FirstOrDefault();
+
+        public Task<string> GetRegisterTemplate()
+            => SimpleCommand.ExecuteQueryFirstColumnAsync<string>(_connectionString,
+                    $"select {nameof(MessageTemplate.Template)} from {TableName} where {nameof(MessageTemplate.Name)}='RegisterTemplate'")
+                .FirstOrDefault();
+
+        public Task<string> GetRequestResetTemplate()
+            => SimpleCommand.ExecuteQueryFirstColumnAsync<string>(_connectionString,
+                    $"select {nameof(MessageTemplate.Template)} from {TableName} where {nameof(MessageTemplate.Name)}='RequestResetTemplate'")
+                .FirstOrDefault();
+    }
+}
