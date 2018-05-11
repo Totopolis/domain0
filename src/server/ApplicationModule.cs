73cf46d3f08c5a6b135bdbce8a13fa18eb3940ad
novinskii <novinskii@smartdriving.io>
2018-05-11 18:31:50 +0300

    register module with repositories

--- src/server/ApplicationModule.cs
+++ src/server/ApplicationModule.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using Autofac;
+using Autofac.Core;
 using AutoMapper;
+using Domain0.FastSql;
+using Domain0.Repository;
 using Domain0.Service;
 using Microsoft.IdentityModel.Tokens;
 
@@ -15,6 +19,19 @@ namespace Domain0
             builder.RegisterType<PasswordGenerator>().As<IPasswordGenerator>().SingleInstance();
             builder.RegisterType<TokenGenerator>().As<ITokenGenerator>().SingleInstance();
             builder.RegisterType<AccountService>().As<IAccountService>().InstancePerLifetimeScope();
+
+            var connectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+            builder.RegisterInstance(connectionString).Named<string>(nameof(connectionString));
+
+            builder.RegisterType<AccountRepository>().As<IAccountRepository>()
+                .WithParameter(new ResolvedParameter((pi, ctx) => pi.ParameterType == typeof(string),
+                    (pi, ctx) => ctx.ResolveNamed<string>(nameof(connectionString)))).SingleInstance();
+            builder.RegisterType<MessageTemplateRepository>().As<IMessageTemplateRepository>().SingleInstance();
+            builder.RegisterType<PermissionRepository>().As<IPermissionRepository>().SingleInstance();
+            builder.RegisterType<RoleRepository>().As<IRoleRepository>().SingleInstance();
+            builder.RegisterType<SmsRequestRepository>().As<ISmsRequestRepository>().SingleInstance();
+            builder.RegisterType<TokenRegistrationRepository>().As<ITokenRegistrationRepository>().SingleInstance();
+
             var settings = new TokenGeneratorSettings
             {
                 Audience = "*",
