0e6ec90c2cd48b073bc918c5ee60798b27a52122
Сергей Кочековский <KOCHEKOVSKII@smartdriving.io>
2018-09-11 17:10:04 +0300

    fix project structure

--- prj/Domain0.Nancy/Service/SqlQueueSmsClientSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain0.Nancy.Service
-{
-    public class SqlQueueSmsClientSettings
-    {
-        public string ConnectionString { get; set; }
-
-        public string QueueName { get; set; }
-    }
-}
--- src/server/SqlQueueSmsClientSettings.cs
+++ src/server/SqlQueueSmsClientSettings.cs
@@ -2,5 +2,8 @@ namespace Domain0.Nancy.Service
 {
     public class SqlQueueSmsClientSettings
     {
+        public string ConnectionString { get; set; }
+
+        public string QueueName { get; set; }
     }
 }
