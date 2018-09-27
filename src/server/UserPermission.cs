9c08d77dc80d2007329e016d68727125e245e480
Сергей Кочековский <KOCHEKOVSKII@smartdriving.io>
2018-09-27 18:13:32 +0300

    RolePermission, UserPermission fix nullable fields in entities

--- src/server/UserPermission.cs
+++ src/server/UserPermission.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
 
-        public int RoleId { get; set; }
+        public int? RoleId { get; set; }
 
         public int ApplicationId { get; set; }
 
