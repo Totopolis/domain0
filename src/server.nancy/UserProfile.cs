0a5723c4782bfa228bea44a3cd8eda0580fe58c5
Сергей Кочековский <KOCHEKOVSKII@smartdriving.io>
2018-10-01 12:46:05 +0300

    profile should have email field

--- src/server.nancy/UserProfile.cs
+++ src/server.nancy/UserProfile.cs
@@ -13,23 +13,30 @@ namespace Domain0.Model
                 FieldSetting<UserProfile>.CreateInt32(1, c => c.Id, (c, v) => c.Id = v),
                 FieldSetting<UserProfile>.CreateString(2, c => c.Name, (c, v) => c.Name = v, c => c.Name?.Length > 0),
                 FieldSetting<UserProfile>.CreateInt64(3, c => (long) c.Phone, (c, v) => c.Phone = v, c => c.Phone != null),
-                FieldSetting<UserProfile>.CreateString(4, c => c.Description, (c, v) => c.Description = v, c => c.Description?.Length > 0),
+                FieldSetting<UserProfile>.CreateString(4, c => c.Email, (c, v) => c.Email = v, c => !string.IsNullOrWhiteSpace(c.Email)),
+                FieldSetting<UserProfile>.CreateString(5, c => c.Description, (c, v) => c.Description = v, c => c.Description?.Length > 0),
             });
 
         /// <summary>
-        /// Идентификатор пользователя.
+        /// User identifier
         /// </summary>
         public int Id { get; set; }
         /// <summary>
-        /// Имя пользователя.
+        /// User name
         /// </summary>
         public string Name { get; set; }
         /// <summary>
-        /// Телефон.
+        /// User phone number
         /// </summary>
         public decimal? Phone { get; set; }
+
         /// <summary>
-        /// Описание пользователя.
+        /// User email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Description
         /// </summary>
         public string Description { get; set; }
     }
