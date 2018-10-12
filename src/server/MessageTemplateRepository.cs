a1bd7a15c9e7c18455b7c707446981736e9602c0
gabbacode <mail2gabba@gmail.com>
2018-10-12 16:55:26 +0300

    improove locale mathcing algorithm

--- src/server/MessageTemplateRepository.cs
+++ src/server/MessageTemplateRepository.cs
@@ -20,24 +20,41 @@ namespace Domain0.FastSql
             CultureInfo culture,
             MessageTemplateType type)
         {
-            return await GetTemplateInternal(name, culture, type) 
-                // fall back to default culture
-                ?? await GetTemplateInternal(name, CultureInfo.CurrentCulture, type);
+            return await GetTemplateInternal(name, culture, type);
         }
 
-        private Task<string> GetTemplateInternal(MessageTemplateName name, CultureInfo culture, MessageTemplateType type)
+        private async Task<string> GetTemplateInternal(MessageTemplateName name, CultureInfo culture, MessageTemplateType type)
         {
-            return SimpleCommand.ExecuteQueryFirstColumnAsync<string>(
+            var templates = await SimpleCommand.ExecuteQueryAsync<MessageTemplate>(
                     connectionString,
                     $"select {nameof(MessageTemplate.Template)} " +
                     $"from {TableName} " +
                     $"where {nameof(MessageTemplate.Name)}      =@p0 " +
-                    $"  and {nameof(MessageTemplate.Locale)}    =@p1 " +
                     $"  and {nameof(MessageTemplate.Type)}      =@p2 ",
                     name.ToString(),
-                    culture.TwoLetterISOLanguageName, 
                     type.ToString())                    
-                .FirstOrDefault();
+                .ToArray();
+
+            return GetTemplateMatch(culture, templates) ??
+                   // fall back to default culture
+                   GetTemplateMatch(CultureInfo.CurrentCulture, templates);
+        }
+
+        private static string GetTemplateMatch(CultureInfo culture, MessageTemplate[] templates)
+        {
+            var fullyCompatibleLocale = templates.FirstOrDefault(t => culture.Name == t.Locale);
+            if (fullyCompatibleLocale != null)
+                return fullyCompatibleLocale.Template;
+
+            var generallyCompatibleLocale = templates.FirstOrDefault(t => culture.TwoLetterISOLanguageName == t.Locale);
+            if (generallyCompatibleLocale != null)
+                return generallyCompatibleLocale.Template;
+
+            var partlyCompatibleLocale = templates.FirstOrDefault(t => t.Locale.StartsWith(culture.TwoLetterISOLanguageName));
+            if (partlyCompatibleLocale != null)
+                return partlyCompatibleLocale.Template;
+
+            return null;
         }
     }
 }
