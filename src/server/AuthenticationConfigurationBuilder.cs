80cc6a8368eebeb3b13ca315dab6c1b9d9b5a339
gabbacode <mail2gabba@gmail.com>
2018-10-27 00:19:21 +0300

    TokenPrefix IgnoreCase

--- src/server/AuthenticationConfigurationBuilder.cs
+++ src/server/AuthenticationConfigurationBuilder.cs
@@ -6,6 +6,8 @@ namespace Domain0.Nancy.Infrastructure
 {
     internal class AuthenticationConfigurationBuilder : IAuthenticationConfigurationBuilder
     {
+        private const string TokenPrefix = "Bearer ";
+
         public AuthenticationConfigurationBuilder(ITokenGenerator tokenGeneratorInstance)
         {
             tokenGenerator = tokenGeneratorInstance;
@@ -15,16 +17,19 @@ namespace Domain0.Nancy.Infrastructure
         {
             var configuration = new StatelessAuthenticationConfiguration(ctx =>
             {
-                var jwtToken = ctx.Request.Headers.Authorization;
+                var authorization = ctx.Request.Headers.Authorization;
 
-                if (string.IsNullOrWhiteSpace(jwtToken))
+                if (string.IsNullOrWhiteSpace(authorization))
                     return null;
+
+                if (!authorization.StartsWith(TokenPrefix, StringComparison.OrdinalIgnoreCase))
+                    return null;
+
+                var jwtToken = authorization.Remove(0, TokenPrefix.Length);
 
                 try
                 {
-                    return tokenGenerator.Parse(jwtToken
-                        .Replace("Bearer ", "")
-                        .Replace("bearer ", ""));
+                    return tokenGenerator.Parse(jwtToken);
                 }
                 catch (Exception)
                 {
