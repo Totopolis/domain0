3f41bd7f6064c71778ae759a00f2834352ebe1ab
gabbacode <mail2gabba@gmail.com>
2019-09-02 16:17:37 +0300

    fix parameter naming

--- src/server/SmsGatewayClient.cs
+++ src/server/SmsGatewayClient.cs
@@ -26,7 +26,7 @@ namespace Domain0.Nancy.Service
             var urlBuilder = new System.Text.StringBuilder();
             urlBuilder.Append($"/sms/{phone}?");
             urlBuilder.Append("message=").Append(Uri.EscapeDataString(message)).Append("&");
-            urlBuilder.Append("naming=").Append(Uri.EscapeDataString(environment)).Append("&");
+            urlBuilder.Append("environmenttoken=").Append(Uri.EscapeDataString(environment)).Append("&");
             urlBuilder.Length--;
 
             var request = new HttpRequestMessage(HttpMethod.Put, urlBuilder.ToString());
