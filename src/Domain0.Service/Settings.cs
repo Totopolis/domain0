1892d5fd4bf91ac8d5253b687c81eb5d170d01a0
Михаил Михайлов <m.mikhailov@smartdriving.io>
2019-10-10 17:27:17 +0300

    Add Dockerfile, Remove "if DEBUG"

--- src/Domain0.Service/Settings.cs
+++ src/Domain0.Service/Settings.cs
@@ -5,22 +5,18 @@ namespace Domain0.Service
 {
     public class Settings
     {
-        public const string DefaultConnectionString =
-            "Data Source=.;Initial Catalog=Domain0;Persist Security Info=True;Integrated Security=True";
+        private const string DefaultHttpUri = "http://localhost";
 
-        public static string ConnectionString => 
-            ConfigurationManager.ConnectionStrings["Database"]?.ConnectionString 
-            ?? DefaultConnectionString;
+        private const string DefaultConnectionString =
+            "Data Source=.;Initial Catalog=Domain0;Persist Security Info=True;Integrated Security=True";
 
         public static Uri Uri =>
             string.IsNullOrEmpty(ConfigurationManager.AppSettings["Url"])
                 ? new Uri(DefaultHttpUri)
                 : new Uri(ConfigurationManager.AppSettings["Url"]);
 
-#if DEBUG
-        public const string DefaultHttpUri = "http://localhost:8880";
-#else
-        public const string DefaultHttpUri = "http://localhost";
-#endif
+        public static string ConnectionString =>
+            ConfigurationManager.ConnectionStrings["Database"]?.ConnectionString
+            ?? DefaultConnectionString;
     }
 }
